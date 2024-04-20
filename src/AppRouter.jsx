09449962f0f782b02d948442b0bf0117af8a401d
victorvictoria-maker <victorvictoria0001@gmail.com>
2024-04-20 14:31:56 +0100

    Implemented error boundary

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,18 +2,16 @@ import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 let Home = lazy(() => import("./pages/Home"));
-
-let About = lazy(() => import("./pages/About"));
 let SingleRepo = lazy(() => import("./pages/SingleRepo"));
 
 const AppRouter = () => {
   return (
     // fallback={<div>Loading...</div>}
+
     <Suspense>
       <Routes>
         <Route path='/' element={<Home />} />
         {/* Nested route for SingleRepo */}
-        <Route path='/about' element={<About />} />
         <Route path='/repositories/:id' element={<SingleRepo />} />
         {/* 404 route */}
         <Route path='*' Component={() => <div>Error</div>} />
--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,23 @@ import App from "./App.jsx";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
 
 // import './index.css'
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </HelmetProvider>
+    <ErrorBoundary
+      fallback={<p>Something went wrong and this is error boundary</p>}
+    >
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </HelmetProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
