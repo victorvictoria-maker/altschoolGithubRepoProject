522190231a7114b5bca6baa83f66df6cad4b5763
victorvictoria-maker <victorvictoria0001@gmail.com>
2024-04-23 13:20:40 +0100

    Fixed persisting error page bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,19 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
 import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = (prop) => {
   return (
     <footer className='bg-gradient-to-r from-purple-800 via-green-800 to-red-800 mt-5 text-white p-4'>
       <Flex justify='space-between' alignItems='center'>
-        <Button
-          colorScheme='green'
-          onClick={() => (window.location.href = "/404")}
-        >
-          Test 404 Page
-        </Button>
+        <Link to='/404'>
+          <Button
+            colorScheme='green'
+            // onClick={() => (window.location.href = "/404")}
+          >
+            Test 404 Page
+          </Button>
+        </Link>
         <Button colorScheme='green' onClick={prop.changeErrorBoundary}>
           Toggle Error Boundary
         </Button>
